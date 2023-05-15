d6875eaa0d626aa79b9da9d99193c6f1b16fa690
sebasmontoyasm <sebasmontoyasm@gmail.com>
2023-05-15 10:24:38 -0500

    startup pedro vuc bat

---
+++ scripts/startupPedroVuc.bat
@@ -0,0 +1,12 @@
+echo Ejecutar PEDRO-VUC
+
+@echo echo Se ejecutara PEDRO-VUC
+:
+@echo off
+
+cd C:\Users\RADIAN\AppData\Local\Programs\UiPath\Studio
+UiRobot.exe -p PEDRO-VUC
+
+TIMEOUT /T 15
+
+exit
