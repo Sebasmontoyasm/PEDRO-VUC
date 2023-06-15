d2d1d46ce947dd8111b2c08e17bbb568fc51d1d1
sebasmontoyasm <sebasmontoyasm@gmail.com>
2023-06-14 19:39:30 -0500

    Ultimos cambios para el flujo nuevo

--- scripts/startupPedroVuc.bat
+++ scripts/startupPedroVuc.bat
@@ -1,5 +1,16 @@
 echo Ejecutar PEDRO-VUC
 
+TASKILL /IM
+UiPath.Service.UserHost.exe /F
+TASKILL /IM
+UiPath.RobotJS.UserHost.exe /F
+TASKILL /IM
+UiPath.Executor.exe /F
+TASKILL /IM
+UiPath.Service.Agent.exe /F
+TASKILL /IM
+UiPath.Service.Assistant.exe /F
+
 @echo echo Se ejecutara PEDRO-VUC
 :
 @echo off
