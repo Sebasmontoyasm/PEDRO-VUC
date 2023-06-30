7af18a58a06b6baa8a97ce87c331a05a7efa1043
sebasmontoyasm <sebasmontoyasm@gmail.com>
2023-06-30 01:05:08 -0500

    Cambios a: Backup, asserts/debug y correcciones a los nuevos asuntos de correo Read, Leido, Registrado para el click a resultado de busqueda.

--- scripts/startupPedroVuc.bat
+++ scripts/startupPedroVuc.bat
@@ -1,23 +1,13 @@
 echo Ejecutar PEDRO-VUC
 
-TASKILL /IM
-UiPath.Service.UserHost.exe /F
-TASKILL /IM
-UiPath.RobotJS.UserHost.exe /F
-TASKILL /IM
-UiPath.Executor.exe /F
-TASKILL /IM
-UiPath.Service.Agent.exe /F
-TASKILL /IM
-UiPath.Service.Assistant.exe /F
-
 @echo echo Se ejecutara PEDRO-VUC
 :
 @echo off
 
+TIMEOUT /T 10
+
 cd C:\Users\PEDRO-VUC\AppData\Local\Programs\UiPath\Studio
 UiRobot.exe -p PEDRO-VUC
-
 TIMEOUT /T 15
 
 exit
